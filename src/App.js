af38d3cfe95a5f2fb03a41317dd0a7983bd53d42
Kurni Manushri <manusri2005@gmail.com>
2025-04-20 13:36:31 +0530

    Fix line ending warnings

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Welcome to My Portfolio</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi, I'm Kurni Manushri. I'm a Software Engineer with a passion for building web applications. 
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          You can find my work and projects below!
+        </p>
       </header>
     </div>
   );
 }
 
 export default App;
+
